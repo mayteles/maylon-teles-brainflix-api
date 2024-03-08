1ed031f7781efc9d3e95261494fbbb4235e0f560
mayteles <maylon.ferreira@icloud.com>
2024-03-07 20:44:42 -0800

    adding path to videos-details

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 800; 
 const DATA_FILE_PATH = path.join(__dirname, 'data', 'videos.json');
 const STATIC_ASSETS_PATH = path.join(__dirname, 'public', 'images');
+const VIDEO_DETAILS_FILE_PATH = path.join(__dirname, 'routes', 'videos-details.json');
 
 app.use(express.json());
 app.use(express.static(STATIC_ASSETS_PATH));
@@ -45,6 +46,39 @@ app.get('/videos/:id', async (req, res) => {
         res.status(404).json({message: 'No video with that id exists'});
     }
 });
+
+// Video details path
+app.get('/videos/:id', async (req, res) => {
+    const videoId = req.params.id;
+
+    try {
+        const videoDetails = await loadVideoDetails(videoId);
+
+        res.json(videoDetails);
+    } catch (error) {
+        console.error('Error fetching video details:', error);
+        res.status(500).json({message: 'Internal Server Error' });
+    }
+})
+
+async function loadVideoDetails(videoId) {
+    try {
+        // Read the contents of videos-details.json
+        const data = await fs.readFile(VIDEO_DETAILS_FILE_PATH, 'utf-8');
+        const videosDetails = JSON.parse(data);
+        
+        // Find the video details based on the provided video ID
+        const videoDetails = videosDetails.find(video => video.id === videoId);
+        
+        if (videoDetails) {
+            return videoDetails;
+        } else {
+            throw new Error('Video not found');
+        }
+    } catch (error) {
+        throw error;
+    }
+}
 
 app.post('/videos', async (req, res) => {
     const newVideo = req.body;
